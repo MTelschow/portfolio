a9c0f50b4dc2f37deb89c1198eb0c6edaced7b4f
Mathis Telschow <mathistelschow@gmail.com>
2023-05-28 19:07:58 +0200

    Change work header

--- frontend_react_portfolio/src/container/Work/Work.jsx
+++ frontend_react_portfolio/src/container/Work/Work.jsx
@@ -38,7 +38,7 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+      <h2 className="head-text">Personal <span>Projects</span></h2>
 
       <div className="app__work-filter">
         {['React', 'Next.js', 'All'].map((item, index) => (
